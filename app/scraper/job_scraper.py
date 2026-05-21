e93fe82a8374c66de1ed331774772dd7e62aa33e
GlkMohan <mohanglk123@gmail.com>
2026-05-21 09:34:59 +0530

    added stable job title nd company scraper

--- app/scraper/job_scraper.py
+++ app/scraper/job_scraper.py
@@ -11,18 +11,37 @@ def scrape_jobs():
 
         page = browser.new_page()
 
-        print("Opening website...")
+        print("Opening RemoteOK...")
 
         page.goto(
-            "https://example.com",
+            "https://remoteok.com/remote-java-jobs",
             timeout=60000
         )
 
-        print("Website opened successfully")
+        print("Website loaded successfully")
 
-        title = page.title()
+        job_titles = page.locator("tr.job h2")
 
-        print("Website Title:", title)
+        companies = page.locator("h3[itemprop='name']")
+
+        title_count = job_titles.count()
+
+        company_count = companies.count()
+
+        print(f"Total Titles Found: {title_count}")
+        print(f"Total Companies Found: {company_count}")
+
+        total = min(title_count, company_count, 5)
+
+        for i in range(total):
+
+            title = job_titles.nth(i).inner_text()
+
+            company = companies.nth(i).inner_text()
+
+            print("----------------------")
+            print("Role:", title)
+            print("Company:", company)
 
         input("Press Enter to close browser...")
 
